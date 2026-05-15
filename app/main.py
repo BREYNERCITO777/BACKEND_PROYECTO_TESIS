e3f2170df1074fb80d3f485f74ef6555799c531d
BREYNERCITO777 <breycruz11@gmail.com>
2026-05-15 13:09:04 -0500

    fix cors for vercel domains

--- app/main.py
+++ app/main.py
@@ -42,6 +42,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origenes_permitidos,
+        allow_origin_regex=r"https://.*\.vercel\.app",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
